338ebd79a951376aed7457fefec6f9148fb70c29
Rahul Kumar <rjrahul1855@gmail.com>
2024-04-10 02:25:02 +0530

    cchecking

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    // origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     // origin: 'http://localhost:5173',
+//     credentials: true
+// }))
+
+app.use(cors())
 
 app.use(express.json())
 // app.use(express.json({limit: "16kb"}))
