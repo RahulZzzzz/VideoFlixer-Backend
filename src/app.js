6a06bfff28b3c65fb1b9bbd8fbca31f0d5a50e53
Rahul Kumar <rjrahul1855@gmail.com>
2024-04-10 14:38:45 +0530

    few changes

--- src/app.js
+++ src/app.js
@@ -6,21 +6,21 @@ import bodyParser from "body-parser";
 const app = express();
 
 // Enable CORS for specific origin and allow credentials
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://video-flixer.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://video-flixer.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     // origin: 'http://localhost:5173',
-//     credentials: true
-// }))
+app.use(cors({
+    // origin: process.env.CORS_ORIGIN,
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json())
 // app.use(express.json({limit: "16kb"}))
