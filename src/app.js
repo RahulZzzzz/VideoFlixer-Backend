b1ec3a8341a6effc9e80c5765806a09abe3155f5
Rahul Kumar <rjrahul1855@gmail.com>
2024-04-10 14:51:37 +0530

    check

--- src/app.js
+++ src/app.js
@@ -7,20 +7,20 @@ const app = express();
 
 // Enable CORS for specific origin and allow credentials
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
-app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     // origin: process.env.CORS_ORIGIN,
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
 
-// app.use(cors())
+app.use(cors())
 
 app.use(express.json())
 // app.use(express.json({limit: "16kb"}))
